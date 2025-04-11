77ae7a19f714f6ed44cf3164ea9bff18b4005412
robinkii <robinkelsickii@gmail.com>
2025-04-11 01:19:06 -0400

    added user schema

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  mindtag: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isAnonymous: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
