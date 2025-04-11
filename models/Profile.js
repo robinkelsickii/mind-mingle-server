f8d270f2a8c73c868ce137764eaf8ff4fc88b104
robinkii <robinkelsickii@gmail.com>
2025-04-11 01:30:19 -0400

    added profiles schema

---
+++ models/Profile.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const User = require('./User'); // Import the User model
+
+const profileSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  profilePicture: {
+    type: String,
+    default: 'https://i.pravatar.cc/{size}', // random image from pravatar.cc
+  },
+  statusMessage: {
+    type: [String],
+    default: [],
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
+  interest: {
+    type: [String],
+    default: [],
+  },
+  diagnoses: {
+    type: [String],
+    default: [],
+  },
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
